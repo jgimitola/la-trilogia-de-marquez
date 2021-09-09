b6336a4a066903f8bf7ce1b8ac7ea93c4a41d510
Miguel Nieto A <39246879+miguelnietoa@users.noreply.github.com>
2021-09-09 17:25:23 -0500

    reformat: tabla transD
    Co-authored-by: Jesús Imitola <jgimitola@users.noreply.github.com>

--- Compiladores/AlgoritmoArbolSintactico/utils.js
+++ Compiladores/AlgoritmoArbolSintactico/utils.js
@@ -1,44 +1,74 @@
 export function getP(index, estadosD, t, a) {
-  let estados = [...estadosD[t]];
-  let pList = [];
-  for (let i = 0; i < estados.length; i++) {
-    if (index[estados[i]] === a) {
-      pList.push(estados[i]);
-    }
-  }
-  return pList;
+	let estados = [...estadosD[t]];
+	let pList = [];
+	for (let i = 0; i < estados.length; i++) {
+		if (index[estados[i]] === a) {
+			pList.push(estados[i]);
+		}
+	}
+	return pList;
 }
 
 export function eqSet(as, bs) {
-  if (as.size !== bs.size) return false;
-  for (let a of as) if (!bs.has(a)) return false;
-  return true;
+	if (as.size !== bs.size) return false;
+	for (let a of as) if (!bs.has(a)) return false;
+	return true;
 }
 
-export function imprimirResultados(index, tranD, alfabeto, estadosD, simboloVacio) {
-  let tablaTranD = {};
-  const simbolosAlfabeto = [...alfabeto];
-  const estadosLetra = [];
-  const posHashtag = index.length - 1;
+export function imprimirResultados(
+	index,
+	tranD,
+	alfabeto,
+	estadosD,
+	simboloVacio
+) {
+	const tablaTranD = [];
+	const simbolosAlfabeto = [...alfabeto];	
+	const posHashtag = index.length - 1;
 
-  for (let i = 0; i < tranD.length; i++) {
-    let estado = String.fromCharCode(i + 65);
-    estadosLetra.push(estado);
-    if (i === 0) estado = '→' + estado;
-    if (estadosD[i].has(posHashtag)) estado = estado + '*';
-    tablaTranD[estado] = {};
-    for (let j = 0; j < simbolosAlfabeto.length; j++) {
-      let simbolo = simbolosAlfabeto[j];
-      if (tranD[i][j] === simboloVacio) {
-        tablaTranD[estado][simbolo] = simboloVacio;
-      } else {
-        tablaTranD[estado][simbolo] = String.fromCharCode(
-          tranD[i][j] + 65
-        );
-      }
-    }
+	/* 
+  tablaTranD SCHEMA  {
+    estado: "A",
+    inicio: true,
+    finalizacion: false,
+    transicion: [{simbolo:"a", destino: "B"},{"b", "B"}]
   }
-  console.log(`Alfabeto = { ${simbolosAlfabeto.join(', ')} }`);
-		console.log(`Estados  = { ${estadosLetra.join(', ')} }`);
-		console.table(tablaTranD);
+  */
+
+	for (let i = 0; i < tranD.length; i++) {
+		const estadoLetra = String.fromCharCode(i + 65);
+
+		const estadoNodo = {};
+		estadoNodo['estado'] = estadoLetra;
+
+		estadoNodo['inicio'] = false;
+		estadoNodo['finalizacion'] = false;
+
+		if (i === 0) estadoNodo['inicio'] = true;
+
+		if (estadosD[i].has(posHashtag)) estadoNodo['finalizacion'] = true;
+
+		estadoNodo['transicion'] = [];
+		for (let j = 0; j < simbolosAlfabeto.length; j++) {
+			const simbolo = simbolosAlfabeto[j];
+			let destino;
+
+			if (tranD[i][j] === simboloVacio) destino = simboloVacio;
+			else destino = String.fromCharCode(tranD[i][j] + 65);
+
+			estadoNodo['transicion'].push({
+				simbolo,
+				destino,
+			});
+		}
+		tablaTranD.push(estadoNodo);
+	}
+	/* console.log(`Alfabeto = { ${simbolosAlfabeto.join(', ')} }`);
+	console.log(`Estados  = { ${estadosLetra.join(', ')} }`);
+	console.table(tablaTranD); */
+
+	console.log(simbolosAlfabeto);
+	console.log(tablaTranD);
+  console.log("##########");
+	return tablaTranD;
 }
