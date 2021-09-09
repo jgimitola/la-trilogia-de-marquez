708096d4d1a693b605f54825871a0e2546951b7a
Miguel Nieto A <39246879+miguelnietoa@users.noreply.github.com>
2021-09-09 10:53:22 -0500

    feat: print table in a better way

--- Compiladores/AlgoritmoArbolSintactico/index.js
+++ Compiladores/AlgoritmoArbolSintactico/index.js
@@ -5,6 +5,8 @@ const r = "a(a|b)*bb";
 
 const r_hash = r + "#";
 
+const simboloVacio = 'Ø';
+
 let index = [];
 for (let i = 0; i < r_hash.length; i++) {
   if (alfabeto.has(r_hash[i]) || r_hash[i] === "#") {
@@ -15,12 +17,11 @@ for (let i = 0; i < r_hash.length; i++) {
 /* Cambiar */
 let primera_pos = new Set([0]); // primera_pos(r#)
 const siguiente_pos = [
+  new Set([1, 2, 3]), // siguiente_pos(0)
   new Set([1, 2, 3]), // siguiente_pos(1)
   new Set([1, 2, 3]), // siguiente_pos(2)
-  new Set([1, 2, 3]), // siguiente_pos(3)
-  new Set([4]),
-  new Set([5]),
-  new Set([]),
+  new Set([4]), // siguiente_pos(3)
+  new Set([5]), // siguiente_pos(4)
 ];
 
 function getP(estadosD, t, a) {
@@ -40,9 +41,27 @@ function eqSet(as, bs) {
   return true;
 }
 
+function printTranD(tranD, alfabeto) {
+  let tablaTranD = {};
+  for (let i = 0; i < tranD.length; i++) {
+    let estado = String.fromCharCode(i + 65);
+    tablaTranD[estado] = {};
+    const simbolosAlfabeto = [...alfabeto];
+    for (let j = 0; j < simbolosAlfabeto.length; j++) {
+      let simbolo = simbolosAlfabeto[j];
+      if (tranD[i][j] == simboloVacio) {
+        tablaTranD[estado][simbolo] = simboloVacio;
+      } else {
+        tablaTranD[estado][simbolo] = String.fromCharCode(tranD[i][j] + 65);
+      }
+    }
+  }
+  console.table(tablaTranD);
+}
+
 function ejecutar() {
-  let estadosD = [];
-  let tranD = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], []];
+  const estadosD = [];
+  const tranD = [[]];
 
   estadosD.push(primera_pos);
 
@@ -56,10 +75,11 @@ function ejecutar() {
       let pos = estadosD.findIndex((el) => eqSet(el, u));
       if (u.size > 0 && pos === -1) {
         estadosD.push(u);
+        tranD.push([]);
       }
 
       if (u.size === 0) {
-        tranD[t][i] = -1; // Vacio
+        tranD[t][i] = simboloVacio; // Vacio
       } else if (pos !== -1) {
         tranD[t][i] = pos; // Ya existe
       } else {
@@ -69,7 +89,7 @@ function ejecutar() {
     }
     t++;
   }
-  console.table(tranD);
+  printTranD(tranD, alfabeto); // Imprime tranD como una tabla, visualizando los estados con letras mayúsculas [A-Z]
 }
 
 ejecutar();
