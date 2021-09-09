fb3a7dd739ce0b117af32591fc2c44e66937553d
Miguel Nieto A <39246879+miguelnietoa@users.noreply.github.com>
2021-09-09 11:41:36 -0500

    feat: indicar estado de inicio y finalización en tabla

--- Compiladores/AlgoritmoArbolSintactico/index.js
+++ Compiladores/AlgoritmoArbolSintactico/index.js
@@ -41,24 +41,28 @@ function eqSet(as, bs) {
   return true;
 }
 
-function imprimirResultados(tranD, alfabeto) {
+function imprimirResultados(tranD, alfabeto, estadosD) {
   let tablaTranD = {};
   const simbolosAlfabeto = [...alfabeto];
-  const estadosLetra = []
+  const estadosLetra = [];
+  const posHashtag = index.length - 1;
+  
   for (let i = 0; i < tranD.length; i++) {
     let estado = String.fromCharCode(i + 65);
     estadosLetra.push(estado);
+    if (i === 0) estado = "→" + estado
+    if (estadosD[i].has(posHashtag)) estado = estado + "*"
     tablaTranD[estado] = {};
     for (let j = 0; j < simbolosAlfabeto.length; j++) {
       let simbolo = simbolosAlfabeto[j];
-      if (tranD[i][j] == simboloVacio) {
+      if (tranD[i][j] === simboloVacio) {
         tablaTranD[estado][simbolo] = simboloVacio;
       } else {
         tablaTranD[estado][simbolo] = String.fromCharCode(tranD[i][j] + 65);
       }
     }
   }
-  
+
   console.log(`Alfabeto = { ${simbolosAlfabeto.join(", ")} }`);
   console.log(`Estados  = { ${estadosLetra.join(", ")} }`);
   console.table(tablaTranD);
@@ -96,7 +100,7 @@ function ejecutar() {
   }
   
   
-  imprimirResultados(tranD, alfabeto); // Imprime tranD como una tabla, visualizando los estados con letras mayúsculas [A-Z]
+  imprimirResultados(tranD, alfabeto, estadosD); // Imprime Alfabeto, Estados y tranD
 }
 
 ejecutar();
